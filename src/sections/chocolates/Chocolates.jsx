8afb9ffc19fd7886df9bf1952399b151d65bb4a2
MiguelHortencio07 <miguehortenco07@gmail.com>
2024-12-01 22:43:16 -0300

    tentei

--- src/sections/chocolates/Chocolates.jsx
+++ src/sections/chocolates/Chocolates.jsx
@@ -1,10 +1,45 @@
+import './Chocolates.scss'
+import {register} from 'swiper/element/bundle'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {useState} from 'react'
+
+register()
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
 const Chocolates = () => {
+
+  const [SlidesPerView, SetSlidePerview] = useState(1)
+
+  const minguel = [
+    <img src={choco1} alt="choco1" className="choco1" />,
+    <img src={choco1} alt="choco1" className="choco1" />,
+    <img src={choco1} alt="choco1" className="choco1" />
+  ]
+
   return (
-    <div>
-      <h1>chocolate</h1>
-      <h2>datebayo</h2>
+    <div className="criachefe">
+            <Swiper
+              SlidesPerView = {SlidesPerView}
+              pagination = {{clickble : true}}
+              navigation
+            >
+              {minguel.map( (item) => (
+                <SwiperSlide>
+                  <img 
+                    src={item.choco1} 
+                      alt="Slider" 
+                      className='slide-minguel'
+                  />
+                  
+                </SwiperSlide>
+              ))}
+            </Swiper>
     </div>
   )
 }
 
 export default Chocolates
+
