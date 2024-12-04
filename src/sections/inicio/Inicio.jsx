89804e3a0ba034c3518da2310f0ba03dd623f6c1
MiguelHortencio07 <miguehortenco07@gmail.com>
2024-12-03 23:28:44 -0300

    blablabla

--- src/sections/inicio/Inicio.jsx
+++ src/sections/inicio/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
       <div className="nnsei">
         <h1 className="title">Bem-vindo à <span className="chococal-text">Chococal </span>!</h1>
         <p className="text-introduction">
-          Somos apaixonados por chocolate e dedicados a transformar os melhores
+          Somos apaixonados por chocolate e dedicados a transformar os melhores 
           ingredientes em experiências únicas. Nossos produtos são criados com
           cuidado, tradição e inovação para adoçar o seu dia e celebrar os
           momentos mais especiais. Descubra o sabor irresistível e a qualidade
